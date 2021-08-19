74a23e619e6b9c7796658f65bc99e60e9070a1b2
Mabel Lawrence <mabelmail3@gmail.com>
2021-08-19 17:47:48 -0400

    hard coded jobs example

--- src/Experience.js
+++ src/Experience.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import resume from './Mabel_Lawrence_Resume.pdf';
+import jobs from './professional_experience';
 
 class Experience extends React.Component {
   render() {
     return (
       <div id="experience">
         <p className="experience-subtitle">Professional Experience</p>
-        <a href={resume} target="_blank" rel="noopener noreferrer">
-          Resume PDF
-        </a>
+        <div className="experience-item">
+          <div className="company-duration">
+            <div className="company">Recreational Equipment Inc.</div>
+            <div className="duration">August 2019 - Present</div>
+          </div>
+          <div className="position-description">
+            <div className="position">
+              Local Experiences Guide and Retail Specialist
+            </div>
+            <div className="job-description">
+              <div className="description-bullet">
+                - Provided feature specifications, fit guidance, and technical
+                advice about hiking, camping, and climbing gear
+              </div>
+              <div className="description-bullet">
+                - Taught lessons on bouldering, compass navigation, and first
+                aid to encourage confidence in the outdoors
+              </div>
+            </div>
+          </div>
+        </div>
+        <button id="resume-button">
+          <a href={resume} target="_blank" rel="noopener noreferrer">
+            Resume PDF
+          </a>
+        </button>
       </div>
     );
   }
---
+++ src/professional_experience.js
@@ -0,0 +1,32 @@
+const jobs = [
+  {
+    company: 'Recreational Equipment Inc.',
+    duration: 'August 2019 - Present',
+    position: 'Local Experiences Guide and Retail Specialist',
+    description: [
+      '- Provided feature specifications, fit guidance, and technical advice about hiking, camping, and climbing gear',
+      '- Taught lessons on bouldering, compass navigation, and first aid to encourage confidence in the outdoors',
+    ],
+  },
+  {
+    company: 'Stand and Build',
+    duration: 'August 2019 - Present',
+    position: 'Technical Designer',
+    description: [
+      '- Produced drawing sets and renderings for retail fixtures, custom furniture, and pop ups using Rhino 3D',
+      '- Wrote 1900 lines of CSS to edit design of Shopify website to include custom pages, features, and styling',
+      '- Developed Arduino code to actuate rotational and translational animations for window displays on 5th Avenue',
+    ],
+  },
+  {
+    company: 'Assorted Theatrical Work',
+    duration: 'June 2016 - August 2018',
+    position: 'Carpentry and Design Intern',
+    description: [
+      '- Interpreted drawings to construct and install scenery for New York Stage and Film and the Hangar Theatre',
+      '- Coordinated with technical directors to safely optimize load-ins and load-outs of all relevant scenic elements',
+    ],
+  },
+];
+
+export default jobs;
