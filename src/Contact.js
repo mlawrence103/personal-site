43c0cd0450d953df47084b660a8d09b86d001d5f
Mabel Lawrence <mabelmail3@gmail.com>
2021-08-25 12:33:45 -0400

    fixed footer

--- src/Contact.js
+++ src/Contact.js
@@ -6,21 +6,23 @@ class Contact extends React.Component {
   render() {
     return (
       <div id="contact">
-        <div id="contact-headers">
-          <p className="contact-title">The Professional Links</p>
-          <p className="contact-title">Email</p>
-        </div>
-        <div id="contact-info">
-          <div id="contact-logos">
-            <a href="https://www.linkedin.com/in/mabellawrence/">
-              <img src={linkedin} alt="linkedin logo" />
-            </a>
-            <a href="https://github.com/mlawrence103">
-              <img src={github} alt="github logo" />
-            </a>
+        <div id="contact-main">
+          <div id="contact-headers">
+            <p className="contact-title">The Professional Links</p>
+            <p className="contact-title">Email</p>
           </div>
-          <div id="email">
-            <a href="ml935@cornell.edu">ml935@cornell.edu</a>
+          <div id="contact-info">
+            <div id="contact-logos">
+              <a href="https://www.linkedin.com/in/mabellawrence/">
+                <img src={linkedin} alt="linkedin logo" />
+              </a>
+              <a href="https://github.com/mlawrence103">
+                <img src={github} alt="github logo" />
+              </a>
+            </div>
+            <div id="email">
+              <a href="ml935@cornell.edu">ml935@cornell.edu</a>
+            </div>
           </div>
         </div>
       </div>
--- src/projects/tastenotwaste.js
+++ src/projects/tastenotwaste.js
@@ -1,86 +1,93 @@
 import React from 'react';
 import tnw_logo from '../images/tnwIconClean.png';
 import tnw_gif from '../images/fridgeRecordingClear.gif';
+import { useHistory } from 'react-router-dom';
 
 const TasteNotWaste = () => {
+  const history = useHistory();
   return (
-    <div id="taste-not-waste">
-      <div className="go-back">
+    <div className="single-project-page">
+      <div className="go-back-top">
         <a href="/">Home</a>
       </div>
-      <div className="project-name">
-        <div id="tnw-name-logo">
-          <img id="tnw-logo" src={tnw_logo} alt="taste not waste logo" />
-          <div id="tnw-name">
-            <h1>Taste Not Waste</h1>
-            <div id="tnw-subheader">
-              <a
-                href="http://www.tastenotwasteapp.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                http://www.tastenotwasteapp.com
-              </a>
-              <a
-                href="https://github.com/GrundoCapstone/Taste-Not-Waste"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                https://github.com/GrundoCapstone/Taste-Not-Waste
-              </a>
+      <div id="taste-not-waste">
+        <div className="project-name">
+          <div id="tnw-name-logo">
+            <img id="tnw-logo" src={tnw_logo} alt="taste not waste logo" />
+            <div id="tnw-name">
+              <h1>Taste Not Waste</h1>
+              <div id="tnw-subheader">
+                <a
+                  href="http://www.tastenotwasteapp.com"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  http://www.tastenotwasteapp.com
+                </a>
+                <a
+                  href="https://github.com/GrundoCapstone/Taste-Not-Waste"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  https://github.com/GrundoCapstone/Taste-Not-Waste
+                </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="project-page-description">
-        <p>
-          Taste Not Waste is a mobile application that reduces food waste by
-          alerting users when their uploaded grocery items will expire and
-          suggesting recipes using the relevant ingredients. (Built with a team
-          of four for the Grace Hopper Program)
-        </p>
-      </div>
-      <div id="tnw-gif-techstack-responsibilities">
-        <div id="tnw-gif">
-          <img src={tnw_gif} alt="gif of taste not waste app in use" />
+        <div className="project-page-description">
+          <p>
+            Taste Not Waste is a mobile application that reduces food waste by
+            alerting users when their uploaded grocery items will expire and
+            suggesting recipes using the relevant ingredients. (Built with a
+            team of four for the Grace Hopper Program)
+          </p>
         </div>
-        <div id="tnw-techstack-responsibilities">
-          <div id="tnw-responsibilities">
-            <p>Personal Contributions</p>
-            <ul>
-              <li>
-                Implemented Google Vision OCR to parse food names from line
-                items from uploaded or scanned receipt
-              </li>
-              <li>
-                Set up pre-populated form in React Native to allow users to view
-                and edit uploaded items before saving them
-              </li>
-            </ul>
+        <div id="tnw-gif-techstack-responsibilities">
+          <div id="tnw-gif">
+            <img src={tnw_gif} alt="gif of taste not waste app in use" />
           </div>
-          <div id="tnw-techstack">
-            <p>Technologies Used:</p>
-            <ul>
-              <li>React Native</li>
-              <li>Redux</li>
-              <li>Firebase</li>
-              <li>Google Vision</li>
-              <li>CSS</li>
-              <li>Edamam API</li>
-            </ul>
+          <div id="tnw-techstack-responsibilities">
+            <div id="tnw-responsibilities">
+              <p>Personal Contributions</p>
+              <ul>
+                <li>
+                  Implemented Google Vision OCR to parse food names from line
+                  items from uploaded or scanned receipt
+                </li>
+                <li>
+                  Set up pre-populated form in React Native to allow users to
+                  view and edit uploaded items before saving them
+                </li>
+              </ul>
+            </div>
+            <div id="tnw-techstack">
+              <p>Technologies Used:</p>
+              <ul>
+                <li>React Native</li>
+                <li>Redux</li>
+                <li>Firebase</li>
+                <li>Google Vision</li>
+                <li>CSS</li>
+                <li>Edamam API</li>
+              </ul>
+            </div>
           </div>
         </div>
+        <div id="tnw-video">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/zAaoZnrvJPU"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
-      <div id="tnw-video">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/zAaoZnrvJPU"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+      <div className="go-back-bottom">
+        <button onClick={() => history.push('/')}>Go Back</button>
       </div>
     </div>
   );
